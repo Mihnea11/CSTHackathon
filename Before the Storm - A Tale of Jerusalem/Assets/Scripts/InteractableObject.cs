60e0b6366c691e33dfa31f701c5e502f19b07276
Lopataru Mihnea <mihnealopataru@gmail.com>
2023-04-02 08:13:57 +0300

    Finished item inspection + new animations

--- Before the Storm - A Tale of Jerusalem/Assets/Scripts/InteractableObject.cs
+++ Before the Storm - A Tale of Jerusalem/Assets/Scripts/InteractableObject.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractableObject : Interactable
 {
+    [SerializeField] 
+    private GameObject itemDescriptions;
+    [SerializeField]
+    private Button closeButton;
+
+    private Transform describedItem;
+
+
     public override void OnFocus()
     {
         Debug.Log(gameObject.name);
@@ -11,11 +20,32 @@ public class InteractableObject : Interactable
 
     public override void OnInteract()
     {
-        Debug.Log("Touched " + gameObject.name);
+        FirstPersonController.CanMove = false;
+
+        itemDescriptions.SetActive(true);
+        describedItem = itemDescriptions.transform.Find(gameObject.name + "-Information");
+
+        describedItem.gameObject.SetActive(true);
+
+        closeButton.onClick.AddListener(CloseButtonClick);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     public override void OnLoseFocus()
     {
         Debug.Log("Left " + gameObject.name);
+    }
+
+    public void CloseButtonClick()
+    {
+        describedItem.gameObject.SetActive(false);
+        itemDescriptions.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        FirstPersonController.CanMove = true;
     }
 }
