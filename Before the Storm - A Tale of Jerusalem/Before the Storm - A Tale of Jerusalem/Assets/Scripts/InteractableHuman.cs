8250336e56abfb35e92996ff0a07b6191dfc9eba
Lopataru Mihnea <mihnealopataru@gmail.com>
2023-04-02 03:54:29 +0300

    Major gameplay changes

---
+++ Before the Storm - A Tale of Jerusalem/Before the Storm - A Tale of Jerusalem/Assets/Scripts/InteractableHuman.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableHuman : Interactable
+{
+    public override void OnFocus()
+    {
+        Debug.Log(gameObject.name);
+    }
+
+    public override void OnInteract()
+    {
+        Debug.Log("Touched " + gameObject.name);
+    }
+
+    public override void OnLoseFocus()
+    {
+        Debug.Log("Left " + gameObject.name);
+    }
+}
